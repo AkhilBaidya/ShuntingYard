3cda271177e1601535b0931f7dda0ee28bf4f2ad
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-26 10:21:42 -0800

    defining functions for the node class

---
+++ node.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+using namespace std;
+
+
+//its own content:
+void node::setVal(char input) {\
+}
+
+char node::getVal() {
+}
+
+//the left node's content:
+void node::setL(node* input) {
+}
+
+node* node::getL() {
+}
+
+//the right node's content:
+void node::setR(node* input) {
+}
+
+node* node::getR() {
+}
+
+//the next node's content:
+void node::setN(node* input) {
+}
+
+node* node::getN() {
+}

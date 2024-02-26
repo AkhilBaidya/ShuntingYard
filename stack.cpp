df636d57e7bf4c5a138c5ac5a7698c5108b5af3d
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-26 10:39:39 -0800

    going to define stack functions

---
+++ stack.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <stack.h>
+
+using namespace std;
+
+//constructor
+stack::stack(){
+}
+
+//destructor
+stack::~stack(){
+}
+
+//functions
+void stack::push(node* input){
+}
+node* stack::pop(){
+}
+node* stack::peek(){
+}

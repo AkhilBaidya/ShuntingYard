9a64ae7d42695319e241c9b67f80e9788f55b561
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-28 10:48:23 -0800

    Shunting Yard conversion successful!

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -15,16 +15,10 @@ int main() {
   char input;
   
   while (cin >> input && input != '|') { //putting cin>>input in the condition suggested by Kevin. Additionally, saw that this syntax is viable online at stack overflow (https://stackoverflow.com/questions/19483126/whats-the-difference-between-whilecin-and-whilecin-num) from user templatetypedef
-    
-    cout << "in loop " << endl;
-    
     node* toAdd = new node(input);
-    cout << "read in " << (int)input << endl;
-
     //https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html (referred to this for ASCII conversions)
     
     if ((int)input >= 48 && (int) input <= 57) { //a number
-      cout << "adding to queue" << endl;
       numbers -> enqueue(toAdd);
       cout << "added to queue" << endl;
     }
@@ -33,12 +27,7 @@ int main() {
       operators -> push(toAdd);
       cout << "added to stack" << endl;
     }
-
-    cout << "still in loop" << endl;
   }
-
-  cout << "done" << endl;
-
 
   //READ OUT POSTFIX NOTATION:
   cout << "Here is the postfix notation (using Shunting Yard algorithm): ";
@@ -46,19 +35,15 @@ int main() {
   node* toAddOp;
 
   //cout the numbers
-  cout << "Going to read out the queue" << endl;
   while (numbers -> isEmpty() == false) {
-    cout << "trying" << endl;
     toAddNum = numbers -> dequeue();
     cout << toAddNum -> getVal() << " ";
   }
-
   //cout the operators
   while (operators -> peek() != NULL) {
     toAddOp = operators -> pop();
     cout << toAddOp -> getVal() << " ";
   }
-
   cout << endl;
   return 0;
 }

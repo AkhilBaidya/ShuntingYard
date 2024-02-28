9443c2a221b4cc83dffdbc0f359e300a2c53f6af
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 21:32:50 -0800

    Add the input to a queue or stack based on the Shunting Yard algorithm

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -10,8 +10,23 @@ int main() {
   queue* numbers = new queue();
   stack* operators = new stack();
 
-  cout << "Please input a mathematical expression seperated by spaces (use integers)" << endl;
+  cout << "Please input a mathematical expression seperated by spaces (use single digit integers)" << endl;
 
+  while (cin) {
+    char input;
+    node* toAdd = new node();
+    cin >> input;
+    toAdd -> setVal(input);
+    
+    if ((int)input >= 0 && (int) input <= 9) { //a number
+      numbers -> enqueue(toAdd);
+    }
+
+    else { //consider the input an operator
+      operators -> push(toAdd);
+    }
+  }
+  
   return 0;
 }
 

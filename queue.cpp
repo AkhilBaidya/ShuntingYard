e23123bc406abec2ba4b4e76ebdfc0df7d2d3c26
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 21:14:40 -0800

    Defined dequeue (planning on destructor using it, but will test if this works)

--- queue.cpp
+++ queue.cpp
@@ -9,14 +9,26 @@ queue::queue() {
 }
 
 queue::~queue() {
+  node* current = header;
+
+  while (current -> getN() != NULL) {
+    current = current -> getN();
+    dequeue();
+  } //remove all elements from the list
 }
 
 void queue::enqueue(node* input) { //add to the end of the linked list
   node* current = header;
 
-  while (current -> next != NULL) {
-    current = current -> next;
+  while (current -> getN() != NULL) {
+    current = current -> getN();
   } //point to the point where the next node is null
 
-  current -> next = input; //put input there
+  current -> setN(input); //put input there
+}
+
+node* queue::dequeue() {
+  node* prevHead = header;
+  header = header -> getN(); //set next as the header (removing the header)
+  return prevHead; //return the previous top
 }

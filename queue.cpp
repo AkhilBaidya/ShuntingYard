1a4a0833028ab7e8ac432a53737ade42b9e3b1cf
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 21:09:18 -0800

    Defined enqueue

--- queue.cpp
+++ queue.cpp
@@ -10,3 +10,13 @@ queue::queue() {
 
 queue::~queue() {
 }
+
+void queue::enqueue(node* input) { //add to the end of the linked list
+  node* current = header;
+
+  while (current -> next != NULL) {
+    current = current -> next;
+  } //point to the point where the next node is null
+
+  current -> next = input; //put input there
+}

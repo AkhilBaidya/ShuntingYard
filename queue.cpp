2564dfea479086904c21d21adc5983e2434d4b9d
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-28 10:45:20 -0800

    Deleted extra stuff

--- queue.cpp
+++ queue.cpp
@@ -19,8 +19,6 @@ queue::~queue() {
 
 void queue::enqueue(node* input) { //add to the end of the linked list
   node** current  = &header;
-  cout << "got head" << endl;
-
   if ((*current) == NULL) {
     (*current) = input;
   }
@@ -29,21 +27,14 @@ void queue::enqueue(node* input) { //add to the end of the linked list
     while ((*current) -> getN() != NULL) {
       node* prevHead = (*current);
       (*current) = prevHead -> getN();
-      cout << "moving on" << endl;
     } //point to the point where the next node is null
-
-    cout << "set the input " << input -> getVal() << endl;
     (*current) -> setN(input); //put input there
-    cout << header -> getVal() << endl;
   }
 }
 
 node* queue::dequeue() {
   node* prevHead = header;
-  cout << "set next" << endl;
   header = prevHead -> getN(); //set next as the header (removing the header)
-  cout << "fine" << endl;
-  cout << "new header: " << prevHead -> getVal() << endl;
   return prevHead; //return the previous top
 }
 

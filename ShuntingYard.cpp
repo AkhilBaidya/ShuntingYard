f58edcfcf543d794bed1185df79f4fc9247666fd
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-28 09:47:09 -0800

    Read out the infix as postfix

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -10,7 +10,8 @@ int main() {
   queue* numbers = new queue();
   stack* operators = new stack();
 
-  cout << "Please input a mathematical expression seperated by spaces (use single digit integers and end your expression with |)" << endl;
+  //READ IN INFIX NOTATION:
+  cout << "Please input a mathematical expression in infix seperated by spaces (use single digit integers and end your expression with |)" << endl;
   char input;
   
   while (cin >> input && input != '|') { //putting cin>>input in the condition suggested by Kevin. Additionally, saw that this syntax is viable online at stack overflow (https://stackoverflow.com/questions/19483126/whats-the-difference-between-whilecin-and-whilecin-num) from user templatetypedef
@@ -34,7 +35,26 @@ int main() {
   }
 
   cout << "done" << endl;
-  
+
+
+  //READ OUT POSTFIX NOTATION:
+  cout << "Here is the postfix notation (using Shunting Yard algorithm): ";
+  char toAddNum;
+  char toAddOp;
+
+  //cout the numbers
+  while (numbers -> dequeue() != NULL) {
+    toAddNum = numbers -> dequeue();
+    cout << toAddNum << " ";
+  }
+
+  //cout the operators
+  while (operators -> peek() != NULL) {
+    toAddOp = operators -> pop();
+    cout << toAddOp << " ";
+  }
+
+  cout << endl;
   return 0;
 }
 

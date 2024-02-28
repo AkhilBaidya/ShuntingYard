136705a8342d81e1f8862ff23ecd5f581d12c2ae
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 22:02:28 -0800

    Tried using while(cin) like with file reading but it doesn't end

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -11,21 +11,30 @@ int main() {
   stack* operators = new stack();
 
   cout << "Please input a mathematical expression seperated by spaces (use single digit integers)" << endl;
-
+  
   while (cin) {
+    
     char input;
-    node* toAdd = new node();
     cin >> input;
-    toAdd -> setVal(input);
+    cout << "in loop " << endl;
+    
+    node* toAdd = new node(input);
+    cout << "read in " << input << endl;
     
     if ((int)input >= 0 && (int) input <= 9) { //a number
       numbers -> enqueue(toAdd);
+      cout << "added to queue" << endl;
     }
 
     else { //consider the input an operator
       operators -> push(toAdd);
+      cout << "added to stack" << endl;
     }
+
+    cout << "still in loop" << endl;
   }
+
+  cout << "done" << endl;
   
   return 0;
 }

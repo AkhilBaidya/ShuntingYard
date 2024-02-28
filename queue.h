377a7d02a1d11340464e58543ddc264e737e0166
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 21:02:49 -0800

    Here are the class details for the queue class

--- queue.h
+++ queue.h
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include "node.h"
 
 using namespace std;
 
 class queue {
 
  public:
+  queue(); //constructor
+  ~queue(); //destructor
 
+  //functions:
+  void enqueue(node*);
+  node* dequeue();
+  
  private:
-
+  node* header;
 };

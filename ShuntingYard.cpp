9706d16ce5ebf342eeb1c5aac25d85ccbbb3f2f1
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-28 09:42:06 -0800

    Fixed cin read in by having the user need to end inputs with |

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -10,12 +10,11 @@ int main() {
   queue* numbers = new queue();
   stack* operators = new stack();
 
-  cout << "Please input a mathematical expression seperated by spaces (use single digit integers)" << endl;
+  cout << "Please input a mathematical expression seperated by spaces (use single digit integers and end your expression with |)" << endl;
+  char input;
   
-  while (cin) {
+  while (cin >> input && input != '|') { //putting cin>>input in the condition suggested by Kevin. Additionally, saw that this syntax is viable online at stack overflow (https://stackoverflow.com/questions/19483126/whats-the-difference-between-whilecin-and-whilecin-num) from user templatetypedef
     
-    char input;
-    cin >> input;
     cout << "in loop " << endl;
     
     node* toAdd = new node(input);
@@ -26,7 +25,7 @@ int main() {
       cout << "added to queue" << endl;
     }
 
-    else { //consider the input an operator
+    else if (input != ' ') { //consider the input an operator
       operators -> push(toAdd);
       cout << "added to stack" << endl;
     }

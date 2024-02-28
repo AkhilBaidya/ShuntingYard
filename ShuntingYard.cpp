187c54e5159c6ac701c528cf8d4876184ab9996d
Akhil Baidya <akhilbaidya101@gmail.com>
2024-02-27 21:25:56 -0800

    Will probably create seperate functions for the binary expression tree portion (since the user can call different options) but not this Shunting Yard application

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -7,5 +7,11 @@ using namespace std;
 
 int main() {
 
+  queue* numbers = new queue();
+  stack* operators = new stack();
+
+  cout << "Please input a mathematical expression seperated by spaces (use integers)" << endl;
+
   return 0;
 }
+

e587a9c768045b25f7d5a955da5a1826ccb19fc7
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-05 10:12:12 -0800

    adding by reference, also simplified the enqueue

--- queue.cpp
+++ queue.cpp
@@ -17,23 +17,41 @@ queue::~queue() {
   } //remove all elements from the list
 }
 
-void queue::enqueue(node* input) { //add to the end of the linked list
-  node** current  = &header;
-  if ((*current) == NULL) {
-    (*current) = input;
+void queue::enqueue(node* &input) { //add to the end of the linked list
+
+  if (header == NULL) {
+    header = input;
   }
 
   else {
-    while ((*current) -> getN() != NULL) {
-      node* prevHead = (*current);
-      (*current) = prevHead -> getN();
-    } //point to the point where the next node is null
-    (*current) -> setN(input); //put input there
+
+    node* current = header;
+
+    cout << "entering while loop" << endl;
+    while (current -> getN() != NULL) {
+      current = current -> getN();
+    }
+    cout << "out" << endl;
+    current -> setN(input); //add to end
   }
+
+  //node** current  = &header;
+  //if ((*current) == NULL) {
+  //(*current) = input;
+  //}
+
+  //else {
+  //while ((*current) -> getN() != NULL) {
+  //node* prevHead = (*current);
+  //(*current) = prevHead -> getN();
+  //} //point to the point where the next node is null
+  //(*current) -> setN(input); //put input there
+  //}
 }
 
 node* queue::dequeue() {
-  node* prevHead = header;
+  node* prevHead = new node();
+  prevHead = header;
   header = prevHead -> getN(); //set next as the header (removing the header)
   return prevHead; //return the previous top
 }

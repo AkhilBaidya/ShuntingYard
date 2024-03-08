7c6b4b2e37aa129623fedf98f580cda1766b6671
Akhil Baidya <akhilbaidya101@gmail.com>
2024-03-07 21:20:32 -0800

    Final Commented Version

--- queue.h
+++ queue.h
@@ -1,3 +1,14 @@
+/*C++ Data Structures: Queue Class
+  by Akhil Baidya
+
+  Date of Submission: 3/7/24
+
+  Notes: This is the header file to the queue class. A queue, here, is a linked list of nodes, where nodes can be inputted and outputted in a "first in, first out" manner.
+
+  Reference:
+  Referred to https://web.cecs.pdx.edu/~karlaf/CS163_Slides/CS163_Topic4.pdf by Professor Fant (put on Canvas) to understand the "first in, first out" nature of queues.
+ */
+
 #include <iostream>
 #include <cstring>
 #include "node.h"
@@ -7,14 +18,14 @@ using namespace std;
 class queue {
 
  public:
-  queue(); //constructor
-  ~queue(); //destructor
+  queue(); //Constructor
+  ~queue(); //Destructor
 
-  //functions:
-  void enqueue(node*);
-  node* dequeue();
-  bool isEmpty();
+  //Functions:
+  void enqueue(node*); //add to queue
+  node* dequeue(); //remove from queue
+  bool isEmpty(); //check if queue is empty
   
  private:
-  node* header;
+  node* header; //the head of the linked list
 };
